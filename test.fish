09e29cec9294483c54cb337a91f8756af523a105
yasu-n <3653124+yasu-n@users.noreply.github.com>
2023-07-11 23:25:43 +0900

    整数１つをコンパイルするコンパイラを作成

---
+++ test.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+function assert
+    set expected $argv[1]
+    set input $argv[2]
+
+    cargo run --quiet -- "$input" > tmp.s
+    cc -o tmp tmp.s -Wa,--noexecstack
+    ./tmp
+    set actual "$status"
+
+    if test "$actual" = "$expected"
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    end
+end
+
+assert 0 0
+assert 42 42
+
+echo OK
